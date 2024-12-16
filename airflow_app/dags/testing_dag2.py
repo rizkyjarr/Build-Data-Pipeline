1978df273d8bc288a4e24ab3a26315bea744dfd9
Rizky Fajar <rizkyjarr@gmail.com>
2024-12-16 19:14:56 +0700

    succesfully generated and ingested data to DB & created group task

--- airflow_app/dags/testing_dag2.py
+++ airflow_app/dags/testing_dag2.py
@@ -1,8 +1,13 @@
 from airflow import DAG
 from airflow.operators.python import PythonOperator
+from airflow.utils.task_group import TaskGroup
 from datetime import datetime, timedelta
-from helpers.generate_dummy import generate_customer, generate_product, generate_region, generate_sales_transactions
-from helpers.insert_data import insert_data_customer, insert_data_product, insert_data_region, insert_sales_transactions
+from helpers.generate_dummy import (
+    generate_customer, generate_product, generate_region, generate_sales_transactions
+)
+from helpers.insert_data import (
+    insert_data_customer, insert_data_product, insert_data_region, insert_sales_transactions
+)
 
 # Airflow DAG Definition
 default_args = {
@@ -13,52 +18,62 @@ default_args = {
 }
 
 with DAG(
-    dag_id="generate_dummy_customer_data_modular",
+    dag_id="generate_biodiesel_data_to_DB",
     default_args=default_args,
-    description="Generate and insert one dummy customer data record into PostgreSQL",
-    schedule_interval=timedelta(minutes=15),  # Every 20 seconds
-    #schedule_interval="@hourly",
+    description="Generate and insert biodiesel data records into PostgreSQL",
+    schedule_interval=timedelta(minutes=15),  # Every 15 minutes
     start_date=datetime(2024, 1, 1),  # Set to a recent date
     catchup=False,
     max_active_runs=1,
 ) as dag:
 
-    generate_data_task = PythonOperator(
-        task_id="generate_customer",
-        python_callable=generate_customer,
-    )
+    # Group tasks for Customer
+    with TaskGroup("customer_tasks", tooltip="Generate and insert customer data") as customer_group:
+        generate_customer_task = PythonOperator(
+            task_id="generate_customer",
+            python_callable=generate_customer,
+        )
+        insert_customer_task = PythonOperator(
+            task_id="insert_customer",
+            python_callable=insert_data_customer,
+        )
+        generate_customer_task >> insert_customer_task
 
-    insert_data_customer_task = PythonOperator(
-        task_id="insert_data_customer",
-        python_callable=insert_data_customer,
-    )
-    generate_data_product_task = PythonOperator(
-        task_id="generate_data_product",
-        python_callable=generate_product, 
-    )
-    insert_data_product_task = PythonOperator(
-        task_id="insert_data_product",
-        python_callable=insert_data_product,
-    )
+    # Group tasks for Product
+    with TaskGroup("product_tasks", tooltip="Generate and insert product data") as product_group:
+        generate_product_task = PythonOperator(
+            task_id="generate_product",
+            python_callable=generate_product,
+        )
+        insert_product_task = PythonOperator(
+            task_id="insert_product",
+            python_callable=insert_data_product,
+        )
+        generate_product_task >> insert_product_task
 
-    generate_data_region_task = PythonOperator(
-        task_id="generate_data_region",
-        python_callable=generate_region,
-    )    
-    
-    insert_data_region_task = PythonOperator(
-        task_id="insert_data_region",
-        python_callable=insert_data_region,      
-    )
+    # Group tasks for Region
+    with TaskGroup("region_tasks", tooltip="Generate and insert region data") as region_group:
+        generate_region_task = PythonOperator(
+            task_id="generate_region",
+            python_callable=generate_region,
+        )
+        insert_region_task = PythonOperator(
+            task_id="insert_region",
+            python_callable=insert_data_region,
+        )
+        generate_region_task >> insert_region_task
 
-    generate_data_sales_transactions_task = PythonOperator(
-        task_id="generate_data_sales_transactions",
-        python_callable=generate_sales_transactions,
-    )    
-    
-    insert_data_sales_transactions_task = PythonOperator(
-        task_id="insert_data_sales_transactions",
-        python_callable=insert_sales_transactions,      
-    )
+    # Group tasks for Sales Transactions
+    with TaskGroup("sales_transactions_tasks", tooltip="Generate and insert sales transactions data") as sales_group:
+        generate_sales_task = PythonOperator(
+            task_id="generate_sales",
+            python_callable=generate_sales_transactions,
+        )
+        insert_sales_task = PythonOperator(
+            task_id="insert_sales",
+            python_callable=insert_sales_transactions,
+        )
+        generate_sales_task >> insert_sales_task
 
-    generate_data_task >> insert_data_customer_task  >> generate_data_product_task >> insert_data_product_task >> generate_data_region_task >> insert_data_region_task >> generate_data_sales_transactions_task >> insert_data_sales_transactions_task
+    # Set dependencies between groups
+    customer_group >> product_group >> region_group >> sales_group
